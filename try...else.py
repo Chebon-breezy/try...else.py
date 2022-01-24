55d861661b543f1cbabfb16a272c10c418268e8d
Chebon-breezy <60389652+Chebon-breezy@users.noreply.github.com>
2022-01-24 15:25:23 +0300

    Add files via upload

---
+++ try...else.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from fileinput import filename
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+try:
+    f = open(filename, 'r')
+except OSError:
+    print("file could not be open")
+else:
+    print("number of lines", sum(1 for line in f))
+f.close()
